f7818a5392af95a3a986ea0637485d8c22f9d72c
Priyankakhot03 <126328828+Priyankakhot03@users.noreply.github.com>
2023-03-01 21:27:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const App=() => {
+    return(
+        <>
+        <div>App</div>
+        <h2>Hello</h2>
+        <p>React</p>
+        <h4>Heyy</h4>
+        </>
+    )
+}
+export default App
