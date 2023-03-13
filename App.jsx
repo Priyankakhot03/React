9596cdf41271f9c489fea98431ba92f1d890eb91
Priyankakhot03 <126328828+Priyankakhot03@users.noreply.github.com>
2023-03-13 18:32:19 +0530

    State obj

--- App.jsx
+++ App.jsx
@@ -1,4 +1,66 @@
 import React from 'react';
+import { Component } from 'react';
+class App extends Component
+{
+    state={
+        userName:"Sam"
+    }
+    render()
+    {
+        return(
+            <>
+            <h1>{this.state.userName}</h1>
+            </>
+        )
+    }
+}
+export default App; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import Prop_func from './Components/Prop_func';
 // import Prop_class from './Components/Prop_class';
 // import Prop_1 from './Props/Prop_1';
@@ -7,24 +69,24 @@ import React from 'react';
 // import Course from './JSON/Course';
 // import "./JSON/global.css";
 // import Course1 from './Course1';
-import { Component } from 'react';
-class App extends Component
-{
-    constructor()
-    {
-        super()
-        this.state={
-            userName:"Mee"
-        }
-    }
-    render()
-    {
-        return(
-            <h1>{this.state.userName}</h1>
-        )
-    }
-}
-export default App;
+// import { Component } from 'react';
+// class App extends Component
+// {
+//     constructor()
+//     {
+//         super()
+//         this.state={
+//             userName:"Mee"
+//         }
+//     }
+//     render()
+//     {
+//         return(
+//             <h1>{this.state.userName}</h1>
+//         )
+//     }
+// }
+// export default App;
 
 
 
